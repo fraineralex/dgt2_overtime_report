52c8bb422cf147219043f189d87a1456a3d79ef3
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-03-11 16:01:03 -0400

    add: the initial files of the module

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'DGT-2 Overtime Report',
+    'version': '1.0.0',
+    'category': 'Human Resources',
+    'summary': 'Exports detailed DGT-2 format overtime reports, including costs, to Excel.',
+    'description': 'The module exports detailed DGT-2 format overtime reports, including costs, to Excel, streamlining compliance and optimizing resource management.',
+    'depends': ['hr_payroll'],
+    'author': 'Frainer Encarnación',
+    'website': 'https://fraineralex.dev',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'LGPL-3'
+}
